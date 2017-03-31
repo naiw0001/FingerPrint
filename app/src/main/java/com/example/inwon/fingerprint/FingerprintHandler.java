7b30db7911d4db0d8df45935a7f84fc15cca35ad
naiw0001 <idc06096@naver.com>
2017-03-31 15:51:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/inwon/fingerprint/FingerprintHandler.java
@@ -0,0 +1,68 @@
+package com.example.inwon.fingerprint;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback{
+
+    private CancellationSignal cancellationSignal;
+    private Context context;
+
+    public FingerprintHandler(Context context){
+        this.context = context;
+    }
+
+    public void startAuth(FingerprintManager manager,
+                          FingerprintManager.CryptoObject cryptoObject) {
+
+        cancellationSignal = new CancellationSignal();
+
+        if (ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.USE_FINGERPRINT) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+    @Override
+    public void onAuthenticationError(int errMsgId,
+                                      CharSequence errString) {
+        Toast.makeText(context,
+                 errString,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationHelp(int helpMsgId,
+                                     CharSequence helpString) {
+        Toast.makeText(context,
+                 helpString,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationFailed() {
+        Toast.makeText(context,
+                "인식 실패",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(
+            FingerprintManager.AuthenticationResult result) {
+
+        Toast.makeText(context,
+                "인식 성공",
+                Toast.LENGTH_LONG).show();
+    }
+
+}
